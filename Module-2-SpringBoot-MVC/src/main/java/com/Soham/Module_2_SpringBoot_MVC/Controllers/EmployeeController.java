fc2d3f7260feeb1ec6e7e2f837ca6bd6d146b094
SohamBhandary <bhandarysoham27@gmail.com>
2026-05-25 12:05:43 +0530

    Explored the persistence Layer and Jpa Repository

--- Module-2-SpringBoot-MVC/src/main/java/com/Soham/Module_2_SpringBoot_MVC/Controllers/EmployeeController.java
+++ Module-2-SpringBoot-MVC/src/main/java/com/Soham/Module_2_SpringBoot_MVC/Controllers/EmployeeController.java
@@ -1,34 +1,40 @@
 package com.Soham.Module_2_SpringBoot_MVC.Controllers;
 
 import com.Soham.Module_2_SpringBoot_MVC.DTOs.EmployeeDTO;
+import com.Soham.Module_2_SpringBoot_MVC.Entities.EmployeeEntity;
+import com.Soham.Module_2_SpringBoot_MVC.Repositories.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/employees")
+@RequiredArgsConstructor
 public class EmployeeController {
+    private final EmployeeRepository employeeRepository;
 //    @GetMapping("/home")
 //    public String testGet(){
 //        return "This is our first controller";
 //    }
     @GetMapping("/{id}")
-    public EmployeeDTO getEmployeeById(@PathVariable Long id){
-        return new EmployeeDTO(id,"soham","@gmail",22,true);
+    public EmployeeEntity getEmployeeById(@PathVariable Long id){
+        return employeeRepository.findById(id).orElseThrow();
     }
     @GetMapping()
-    public String getEmployee(@RequestParam Integer age){
-        return "Hi age" + age;
+    public  List<EmployeeEntity> getAllEmployee(){
+        return employeeRepository.findAll();
     }
-
+//
     @PostMapping
-    public String createNewEmployee(){
-        return "Hello";
+    public EmployeeEntity createNewEmployee(@RequestBody EmployeeEntity employeeEntity){
+        return employeeRepository.save(employeeEntity);
     }
-
-    @PutMapping
-    public String updateEmp(){
-        return "Update";
-    }
+//
+//    @PutMapping
+//    public String updateEmp(){
+//        return "Update";
+//    }
 
 }
---
+++ Module-2-SpringBoot-MVC/src/main/java/com/Soham/Module_2_SpringBoot_MVC/Entities/EmployeeEntity.java
@@ -0,0 +1,17 @@
+package com.Soham.Module_2_SpringBoot_MVC.Entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name="Employees")
+@Data
+public class EmployeeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private   Long id;
+    private  String name;
+    private  String email;
+    private  Integer age;
+    private  Boolean isActive;
+}
