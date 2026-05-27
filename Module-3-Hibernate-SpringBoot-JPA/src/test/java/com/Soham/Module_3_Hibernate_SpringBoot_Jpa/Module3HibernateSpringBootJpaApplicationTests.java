75511805bf798ec7d4411437844b38d1ccb4fde7
SohamBhandary <bhandarysoham27@gmail.com>
2026-05-27 21:23:37 +0530

    Explored Hibernate ORM,JPA,Entities,Tables, indexs and unique constriant

---
+++ Module-3-Hibernate-SpringBoot-JPA/src/test/java/com/Soham/Module_3_Hibernate_SpringBoot_Jpa/Module3HibernateSpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Soham.Module_3_Hibernate_SpringBoot_Jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module3HibernateSpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
