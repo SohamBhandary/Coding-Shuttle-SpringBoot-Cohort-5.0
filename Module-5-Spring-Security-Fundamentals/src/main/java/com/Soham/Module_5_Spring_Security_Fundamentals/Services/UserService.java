71e03b6ba52d9e26a0c7e2343076cabb3128733b
SohamBhandary <bhandarysoham27@gmail.com>
2026-06-06 18:43:04 +0530

    Learned about UserDetails,UserDetails Service

---
+++ Module-5-Spring-Security-Fundamentals/src/main/java/com/Soham/Module_5_Spring_Security_Fundamentals/Services/UserService.java
@@ -0,0 +1,19 @@
+package com.Soham.Module_5_Spring_Security_Fundamentals.Services;
+
+import com.Soham.Module_5_Spring_Security_Fundamentals.Repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow();
+    }
+}
