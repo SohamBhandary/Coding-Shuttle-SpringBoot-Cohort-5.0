e3961f76897527a2db1344a803f4d0c82c3aedb8
SohamBhandary <bhandarysoham27@gmail.com>
2026-06-15 11:09:51 +0530

    Security Methods annoatations

--- Module-6-Spring-Security-Advanced/src/main/java/com/Soham/Module_5_Spring_Security_Fundamentals/Config/WebSecurityConfig.java
+++ Module-6-Spring-Security-Advanced/src/main/java/com/Soham/Module_5_Spring_Security_Fundamentals/Config/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -23,6 +24,7 @@ import static com.Soham.Module_5_Spring_Security_Fundamentals.Entities.enums.Rol
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
 
     private final JWTAuthFIlter jwtAuthFIlter;
--- Module-6-Spring-Security-Advanced/src/main/java/com/Soham/Module_5_Spring_Security_Fundamentals/Controller/PostController.java
+++ Module-6-Spring-Security-Advanced/src/main/java/com/Soham/Module_5_Spring_Security_Fundamentals/Controller/PostController.java
@@ -4,6 +4,8 @@ import com.Soham.Module_5_Spring_Security_Fundamentals.DTOs.PostDTO;
 import com.Soham.Module_5_Spring_Security_Fundamentals.Services.PostServiceImpl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,11 +18,13 @@ public class PostController {
     private final PostServiceImpl postService;
 
     @GetMapping
+    @Secured({"ROLE_USER", "ROLE_ADMIN"})
     public List<PostDTO> getAllPosts() {
         return postService.getAllPosts();
     }
 
     @GetMapping("/{postId}")
+    @PreAuthorize("@postSecurity.isOwnerOfPost(#postId)")
     public PostDTO getPostById(@PathVariable Long postId) {
         return postService.getPostById(postId);
     }
