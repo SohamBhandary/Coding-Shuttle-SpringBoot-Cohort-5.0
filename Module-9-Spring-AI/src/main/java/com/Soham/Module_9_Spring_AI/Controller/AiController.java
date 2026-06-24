f58453c6410a84d0ac95740b30c54a1e03c7d160
SohamBhandary <bhandarysoham27@gmail.com>
2026-06-24 21:01:13 +0530

    Explored vector embeddings, data ingestions and similarity serach

--- Module-9-Spring-AI/src/main/java/com/Soham/Module_9_Spring_AI/Controller/AiController.java
+++ Module-9-Spring-AI/src/main/java/com/Soham/Module_9_Spring_AI/Controller/AiController.java
@@ -3,9 +3,10 @@ package com.Soham.Module_9_Spring_AI.Controller;
 
 import com.Soham.Module_9_Spring_AI.Service.AiService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ai.document.Document;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +19,31 @@ public class AiController {
     @GetMapping("/joke")
     public String joke(){
       return   aiService.getJoke("cricket");
+
+
+    }
+
+    @GetMapping("/embed")
+    public float[] embed(){
+
+         var res=  aiService.getEmbedding("cricket");
+
+        return res;
+
+
+    }
+
+    @PostMapping("/ingest")
+    public String ingestData(@RequestBody String text) {
+        aiService.ingestDatatoVectorStore(text);
+        return "Data ingested successfully!";
+    }
+
+    @PostMapping("/similarity")
+    public List<Document> similartySerach(@RequestBody String text) {
+        System.out.println("ok");
+       return aiService.similaritySearch(text);
+
     }
 
 
--- Module-9-Spring-AI/src/main/java/com/Soham/Module_9_Spring_AI/Service/AiService.java
+++ Module-9-Spring-AI/src/main/java/com/Soham/Module_9_Spring_AI/Service/AiService.java
@@ -5,9 +5,14 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 @Service
 @RequiredArgsConstructor
@@ -15,10 +20,28 @@ import java.util.Map;
 public class AiService {
 
     private final ChatClient chatClient;
+    private final EmbeddingModel embeddingModel;
+    private final VectorStore vectorStore;
 
-    public String getJoke(String topic){
 
-        String systemPrompt= """
+    public float[] getEmbedding(String text) {
+
+        return embeddingModel.embed(text);
+    }
+
+    public void ingestDatatoVectorStore(String text){
+        Document document= new Document(text);
+        vectorStore.add(List.of(document));
+    }
+
+    public List<Document> similaritySearch(String text){
+      return   vectorStore.similaritySearch(text);
+    }
+
+
+    public String getJoke(String topic) {
+
+        String systemPrompt = """
                 
                 You are a sractatic joker make 4 lines of peoetic joke,
                 you will stricly stick to the topic as well, make it about politics
@@ -26,16 +49,15 @@ public class AiService {
                 
                 """;
 
-        PromptTemplate promptTemplate= new PromptTemplate(systemPrompt);
-        String rendertext=promptTemplate.render(Map.of("topic",topic));
+        PromptTemplate promptTemplate = new PromptTemplate(systemPrompt);
+        String rendertext = promptTemplate.render(Map.of("topic", topic));
 
 
+        var response = chatClient.prompt()
 
-     var response=   chatClient.prompt()
+                .user(rendertext).call().chatClientResponse();
 
-        .user(rendertext).call().chatClientResponse();
-
-     return  response.chatResponse().getResult().getOutput().getText();
+        return response.chatResponse().getResult().getOutput().getText();
 
 
     }
