5eedda6b4b95333e611cf37f47f9832647feac70
SohamBhandary <bhandarysoham27@gmail.com>
2026-07-15 12:22:31 +0530

    Tomcat Threading model

---
+++ Module-18-MultiThreading-Async-Task-Scheduling/src/main/java/com/soham/Module_18_MultiThreading_Async_Task_Scheduling/MyController.java
@@ -0,0 +1,27 @@
+package com.soham.Module_18_MultiThreading_Async_Task_Scheduling;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class MyController {
+
+    private final ThreadPoolTaskExecutor threadPoolTaskExecutor;
+    private final StudentService studentService;
+
+    @GetMapping("/info")
+    public ResponseEntity<Student> getStudentInfo() {
+
+
+        log.info("starting.. {}", Thread.currentThread().getName());
+        Student student = studentService.getStudentInfo();
+
+        return ResponseEntity.ok(student);
+    }
+}
---
+++ Module-18-MultiThreading-Async-Task-Scheduling/src/main/java/com/soham/Module_18_MultiThreading_Async_Task_Scheduling/Student.java
@@ -0,0 +1,15 @@
+package com.soham.Module_18_MultiThreading_Async_Task_Scheduling;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private String name;
+    private String college;
+    private String id;
+}
+
